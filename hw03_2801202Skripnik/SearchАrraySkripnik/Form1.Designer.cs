31db709314a710f50d3c5cdea53a4792e541707f
0l1vka <hiphopd4@gmail.com>
2021-01-28 15:45:43 +0200

    Сделана 1 задача

--- hw03_2801202Skripnik/SearchАrraySkripnik/Form1.Designer.cs
+++ hw03_2801202Skripnik/SearchАrraySkripnik/Form1.Designer.cs
@@ -37,8 +37,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
@@ -83,7 +81,7 @@
             // 
             this.button4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button4.Location = new System.Drawing.Point(1084, 261);
+            this.button4.Location = new System.Drawing.Point(1084, 238);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(199, 47);
             this.button4.TabIndex = 3;
@@ -95,7 +93,7 @@
             // 
             this.button5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button5.Location = new System.Drawing.Point(1084, 314);
+            this.button5.Location = new System.Drawing.Point(1084, 311);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(199, 47);
             this.button5.TabIndex = 4;
@@ -139,22 +137,6 @@
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 8;
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1084, 228);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Число =";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(1151, 228);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 10;
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
@@ -179,8 +161,6 @@
             this.ClientSize = new System.Drawing.Size(1312, 422);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -209,8 +189,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox3;
     }
---
+++ hw03_2801202Skripnik/SearchАrraySkripnik/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SearchАrraySkripnik
+{
+    public partial class Form1 : Form
+    {   
+        
+        
+        Random rnd = new Random();
+        double[] arr, arr1, arr2;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+        public void displayArray(ref double[] arr)
+        {
+            var index = 0;
+            foreach (var el in arr)
+            {
+                listBox1.Items.Add("el: " + el + " index: " + index);
+                index++;
+            }
+        }
+
+
+        public void halfBiggerArray(int n, ref double[] arr)
+        {
+            int n1 = 0, n2 = 0;
+            for (int i = 0; i < n / 2; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    n1++;
+
+                }
+                if (arr[((n - n / 2) + i)] % 2 == 0)
+                {
+                    n2++;
+                }
+            }
+            if (n1 != n2)
+            {
+                listBox1.Items.Add("amount of odd numbers the same");
+            }
+
+            else if (n1 > n2)
+            {
+                listBox1.Items.Add("in first part of array amount of odd numbers bigger");
+            }
+            else
+            {
+                listBox1.Items.Add("in second part of array amount of odd numbers bigger");
+            }
+            if (n % 2 == 1)
+            {
+                listBox1.Items.Add("The middle number of the array is ignored");
+            };
+        }
+
+
+        public void maxArray(int n, double[] arr, ref double max,int gmin,int gmax)
+        {
+            for (int i = gmin; i < gmax; i++)
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+            }
+        }
+
+
+        public void maxBiggerArray(int n, double[] arr, ref Boolean all_bigger, double max, int gmin, int gmax)
+        {   double max_all = arr[0];
+            maxArray(n, arr, ref max_all, gmin, gmax);
+            if (max_all> max)
+            {
+                all_bigger = true;
+            }
+            listBox1.Items.Add("max of all bigger than max of 2/3: " + all_bigger);
+        }
+
+
+        public void sortedArray(int n, double[] arr, ref Boolean sorted)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[i] < arr[i - 1])
+                {
+                    sorted = false;
+                }
+            }
+            listBox1.Items.Add("sorted: " + sorted);
+        }
+
+
+        public void signChanges(int n, double[] arr, ref int count)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                if ((arr[i] < 0 & arr[i - 1] > 0) || (arr[i] > 0 & arr[i - 1] < 0))
+                {
+                    count++;
+
+                }
+            }
+            listBox1.Items.Add("count of sign changing: " + count);
+        }
+
+
+        public void Changes(int n, double[] arr, ref int count)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                if ((arr[i] < 0 & arr[i - 1] > 0) || (arr[i] > 0 & arr[i - 1] < 0))
+                {
+                    count++;
+
+                }
+            }
+            listBox1.Items.Add("count of sign changing: " + count);
+        }
+        public void minT(int n, ref double min1,double[] arr1)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                if (min1 > arr1[i])
+                {
+                    min1 = arr1[i];
+                }
+            }
+            listBox1.Items.Add("lowest day temp: " + min1);
+        }
+
+
+        public void minTDisplay(int n, ref double min1, double[] arr1)
+        {
+            listBox1.Items.Add("lowest day temp: " + min1);
+        }
+
+
+        public void LowerZero(int n, ref int zero2)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (zero2 == -1)
+                {
+                    if (0 > arr2[i])
+                    {
+                        zero2 = i;
+                    }
+                }
+            }
+            if (zero2 != -1)
+            {
+                listBox1.Items.Add("first time when morning t was lower than o is day index: "+zero2);
+            }
+            else {
+                listBox1.Items.Add("it wasn't t lower than zero");
+            }
+        }
+
+        public void avT(int n, ref double av, double[] arr2)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                av += arr2[i];
+            }
+            av /= n;
+            listBox1.Items.Add("avarage morning t: " + av);
+        }
+
+        public void closeToAvT(int n, ref double min2, double[] arr2,ref double lowest,double av)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (lowest > Math.Abs((double)arr2[i] - av))
+                {
+                    min2 = i;
+                    lowest = Math.Abs((double)arr2[i] - av);
+                }
+            }
+            listBox1.Items.Add("day when morning t the closest to avarage t: day index : " + min2);
+        }
+
+
+        public void dif(int n, double[] arr1, double[] arr2)
+        {
+            double[] arr3=new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr3[i] = Math.Abs(arr1[i] - arr2[i]);
+            }
+            listBox1.Items.Add("ARR3(absolute number of arr1 -arr2): ");
+            displayArray(ref arr3);
+        }
+
+        public void tGrewUp(int n, double[] arr1,ref int count_increase1)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                if (i < n / 2 && arr1[i] > arr1[i - 1])
+                {
+                    count_increase1++;
+
+                }
+            }
+            listBox1.Items.Add("day t grew up: " + count_increase1 + " times");
+        }
+
+        public void biggerT(int n, double t, ref int count2)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                if (arr2[i] > t && i > n / 2)
+                {
+                    count2++;
+                }
+            }
+            listBox1.Items.Add("morming temp. was bigger than 't': " + count2 + " times");
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            /*Заповнити масив з n елементів випадковими цілими числами в діапазоні 
+             * [10; 50]. Написати метод, що визначає, в якій половині масиву більше 
+             * парних чисел: в першій або другій. Продемонструвати його роботу.*/
+            int n = int.Parse(textBox1.Text);
+            fillArray(n,51,10, ref arr);
+            displayArray(ref arr);
+            halfBiggerArray(n,ref arr);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            /*Заповнити масив з n елементів (n кратно трьом) випадковими дійсними
+             * числами в діапазоні [10; 20] з точністю до одного знака після коми.
+             * Написати методи та продемонструвати їх роботу:
+             * Визначити максимальне значення в середній третині масиву.
+             * Чи є в усьому масиві числа, більше знайденого максимального?*/
+            ;
+            int n = int.Parse(textBox1.Text);
+            fillArray(n,21,10, ref arr);
+            double max = arr[n / 3];
+            Boolean all_bigger = false;
+            maxArray(n,arr,ref max,(n/3),(n/3*2));
+            displayArray(ref arr);
+            maxBiggerArray(n,arr,ref all_bigger,max,0,n);
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bool sorted = true;
+            listBox1.Items.Clear();
+            /*Заповнити масив з n елементів випадковими цілими числами в діапазоні [-10; 10].
+             * Написати метод та продемонструвати його роботу:
+             * Визначити, чи є він упорядкованим за зростанням.*/
+            ;
+            int n = int.Parse(textBox1.Text);
+            fillArray(n, 11, -10, ref arr);
+            displayArray(ref arr);
+            sortedArray(n,arr,ref sorted);
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            /*Заповнити масив A з n елементів випадковими цілими числами в діапазоні [-10; 10].
+             * Написати метод та продемонструвати його роботу:
+             * Визначити, скільки разів елементи масиву при перегляді від його початку змінюють знак.
+             * Наприклад, в масиві {10, -4, 2, 5, -4, -8} знак змінюється 3 рази.*/
+            ;
+            int n = int.Parse(textBox1.Text);
+            int count = 0;
+            fillArray(n, 11, -10, ref arr);
+            displayArray(ref arr);
+            signChanges(n, arr, ref count);
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            /*На уроці валеології ліцеїсти 9 класу задали провести спостереження за температурою повітря за останні 2 тижні. Учень вимірював температуру вдень (о 14:00) і вранці (о 6:00). Вам необхідно написати методи, що дозволяють
+             * сформувати 2 масиву спостережень, заповнивши дані за допомогою датчика випадкових в інтервалі від -5 до 10 градусів (використовувати вже створений);
+             * масиви спостережень вивести на форму за допомогою елементів управління ListBox(використовувати вже створений);
+             * визначити, в які дні була мінімальна денна температура і вивести номери цих днів на форму за допомогою елемента управління ListBox;(два методи один визначає мінімальне значення, а інший друкує знайдені значення)
+             * визначити, в який день ранкова температура вперше опустилася нижче 0;
+             * визначити день, ранкова температура в який була найбільш близькою до середнього значення ранкової температури за весь період спостережень;(два методи – середня температура і найближче значення)
+             * сформувати третій масив, який буде містити різниця між денною і ранкової температурами і вивести отриманий масив на форму за допомогою елемента управління ListBox;
+             * визначити, скільки разів за перший тиждень спостерігалося підвищення денної температури в порівнянні з попереднім днем;
+             * визначити, кількість днів за останній тиждень, коли ранкова температура була вище, ніж t градусів (температура t вводиться з клавіатури).*/
+            int n = 14;
+            fillArray(n, 11, -5, ref arr1);
+            fillArray(n, 11, -5, ref arr2);
+            double t = double.Parse(textBox3.Text);
+            double min1 = arr1[0];
+            int zero2=-1;
+            double av = arr2[0];
+            int count_increase1 = 0;
+            int count2 = 0;
+            double lowest = 15;
+            double min2 = 0;
+            listBox1.Items.Add("ARR1(14): ");
+            displayArray(ref arr1);
+            listBox1.Items.Add("ARR2(6): ");
+            displayArray(ref arr2);
+            dif(n,arr2,arr1);
+            minT(n, ref min1, arr1);
+            LowerZero(n, ref zero2);
+            avT(n, ref av, arr2);
+            closeToAvT(n, ref min2, arr2, ref lowest, av);
+            tGrewUp(n,arr1,ref count_increase1);
+            biggerT(n,t,ref count2);
+        }
+    }
+}
