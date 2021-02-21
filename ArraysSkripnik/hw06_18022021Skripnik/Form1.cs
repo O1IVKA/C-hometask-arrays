8c6f5b59480f415f8e4218b8ff145df693ffc93c
0l1vka <hiphopd4@gmail.com>
2021-02-21 14:04:30 +0200

    сделана 4 задача

--- ArraysSkripnik/hw06_18022021Skripnik/Form1.cs
+++ ArraysSkripnik/hw06_18022021Skripnik/Form1.cs
@@ -55,5 +55,79 @@ namespace hw06_18022021Skripnik
             listBox1.Items.Add("B:");
             display(arrb);
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            /*Дано два масиви одного розміру, в яких немає нульових елементів. Отримати третій масив, кожен елемент якого дорівнює 1, 
+             * якщо елементи заданих масивів з тим же номером мають однаковий знак, і дорівнює нулю в іншому випадку.*/
+            int n = int.Parse(textBox1.Text);
+            int[] arra = new int[n];
+            int[] arrb = new int[n];
+            int[] arrс = new int[n];
+            bool sim = true;
+            for (int i = 0; i < n; i++)
+            {
+                arra[i] = rnd.Next(-10, 11);
+                arrb[i] = rnd.Next(-10, 11);
+                if ((arra[i] >= 0 && arrb[i] < 0) || (arra[i] < 0 && arrb[i] >= 0))
+                {
+                    sim = false;
+                }
+            }
+                for (int i = 0; i < n; i++)
+            {
+                if (sim == false)
+                    {
+                        arrс[i] = 0;
+                    }
+                else
+                {
+                    arrс[i] = 1;
+                }
+            }
+            
+            listBox1.Items.Add("A:");
+            display(arra);
+            listBox1.Items.Add("B:");
+            display(arrb);
+            listBox1.Items.Add("C:");
+            display(arrс);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            /*У масиві з 30 елементів числа утворюють неспадна послідовність. Знайти кількість різних чисел в масиві.*/
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            /*Дан масив. Переписати його елементи в інший масив такого ж розміру наступним чином: спочатку повинні йти 
+             * всі негативні елементи, а потім всі інші. Використовувати тільки один прохід по початковому масиву*/
+            int n = int.Parse(textBox1.Text);
+            int[] arra = new int[n];
+            int[] arrb = new int[n];
+            int count = 0;
+            for (int i = 0, j = n - 1, ij = 0; i < n; i++)
+            {
+                arra[i] = rnd.Next(-100, 101);
+                if (arra[i] < 0)
+                {
+                    arrb[ij] = arra[i];
+                    ij++;
+                }
+                else {
+                    arrb[j] = arra[i];
+                    j--;
+                }
+            }
+            listBox1.Items.Add("A:");
+            display(arra);
+            listBox1.Items.Add("B:");
+            display(arrb);
+        }
     }
 }
